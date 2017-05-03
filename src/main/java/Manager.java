daa60292328522899f4a4954c6e852ed963c56ef
Vescatur <ivan@edmi.nl>
2017-05-03 14:10:24 +0200

    no message

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -2,4 +2,24 @@
  * Created by Ivan on 03/05/2017.
  */
 public class Manager {
+    //variablen
+    //SimulatorManager
+    //Array<ARouteBerekening>
+
+    //constructor
+    //voeg simulatorManager toe.
+
+    //functies
+    //LeesXmlOrder(XMLbestand)->SimulatieManager:SetOrder(Order)
+    //getRouteBerekening
+    //setAlgoritme(Enum:Algoritme) ->SimulatieManager:setAlgoritme(Algoritme)
+
+    //BerekenPauze() -> SimulatieManager:BerekenPauze()
+    //BerekenStop() -> SimulatieManager:BerekenStop()
+    //BerekenDoorgaan() -> SimulatieManager:BerekenDoorgaan()
+    //BerekenStart() -> SimulatieManager:BerekenStart() [returned Array<RouteBerekening>]
+
+
+
+
 }
--- src/main/java/SimulatorManager.java
+++ src/main/java/SimulatorManager.java
@@ -2,4 +2,20 @@
  * Created by Ivan on 03/05/2017.
  */
 public class SimulatorManager {
+    //variablen
+    //Manager
+    //Algoritme
+    //Array<Order>
+
+    //constructor
+
+    //functies
+    //setAlgoritme(Algoritme)
+    //setOrder
+
+    //BerekenPauze
+    //BerekenStop
+    //BerekenDoorgaan
+
+    //BerekenStart()return Array<Routeberekening>
 }

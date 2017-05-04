c129f86ce7648cfe72c0edf52bca09ab903d0e9c
Aaron van de Bunt <aaron.vd.bunt@outlook.com>
2017-05-04 10:46:55 +0200

    no message

--- src/main/java/SimulatorTSP/SimulatorManager.java
+++ src/main/java/SimulatorTSP/SimulatorManager.java
@@ -2,24 +2,37 @@ package SimulatorTSP;
 
 import SimulatorTSP.Algoritmes.Algoritme;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
 /**
  * Created by Ivan on 03/05/2017.
  */
 public class SimulatorManager {
     //VERIABLEN
-    //SimulatorTSP.SimulatorTSP.Algoritmes
-    //Array<SimulatorTSP.Order>
+    private Algoritme algoritme;
+    private ArrayList<Order> orderArray;
 
     //CONSTRUCTOR
+    public SimulatorManager(){
 
+    }
 
     //FUNCTIES
-    public Algoritme setAlgoritme(Algoritme algoritme){return null;};
-    public Order setOrder(Order order){return null;};
+    public Algoritme setAlgoritme(Algoritme algoritme){
+        return null;
+    }
 
+    public Order setOrder(Order order){
+        return null;
+    }
 
-    //BerekenStop
+    public void BerekenStop(){
 
-    //BerekenStart()return Array<Routeberekening>
+    }
 
+    public void BerekenStart(){
+        //return Array<Routeberekening>
+    }
 }

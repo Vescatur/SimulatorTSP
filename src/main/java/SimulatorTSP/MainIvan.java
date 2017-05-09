7e77daaa31c06bccc7d3c5ab72440b9ea782f4e5
Vescatur <ivan@edmi.nl>
2017-05-09 15:25:46 +0200

    no message

--- src/main/java/SimulatorTSP/MainIvan.java
+++ src/main/java/SimulatorTSP/MainIvan.java
@@ -3,6 +3,7 @@ package SimulatorTSP;
 import SimulatorTSP.Algoritmes.Algoritme;
 import SimulatorTSP.Algoritmes.AlgoritmeGreedy;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -12,6 +13,7 @@ import java.util.Arrays;
 public class MainIvan {
     public static void main(String[] args){
         XMLReader reader = new XMLReader();
-        System.out.println(Arrays.toString(reader.getOrder().toArray()));
+        ArrayList<ArrayList<Product>> orderArray = reader.ReadXmlFile(new File("namen.xml"));
+        System.out.println(Arrays.toString(orderArray.toArray()));
     }
 }

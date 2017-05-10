78c5ad7d81b1c8bd48a348e86ad7b57d7de0f1db
Aaron van de Bunt <aaron.vd.bunt@outlook.com>
2017-05-10 10:35:37 +0200

    no message

--- src/main/java/SimulatorTSP/Algoritmes/_2Opt.java
+++ src/main/java/SimulatorTSP/Algoritmes/_2Opt.java
@@ -12,14 +12,16 @@ public class _2Opt extends Algoritme{
 
     public ArrayList<Product> BerekenRoute(ArrayList<Product> order) {
         ArrayList<Product> route = new ArrayList<Product>();
+        //lengte van array
+        int grootte = route.size();
+        //verbeter
+        int verbeter = 0;
 
-        for(Product product: order) {
-            product.getHoogte();
-            }
 
-            order.remove(product);
-//
+
+
+        return null;
         }
-        return route;
+
     }
-}
+

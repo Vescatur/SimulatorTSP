166290b7638bc8dbbbc49484c915c1c7460d05a1
Vescatur <ivan@edmi.nl>
2017-05-10 10:17:41 +0200

    no message

--- src/test/java/TestLeesXML.java
+++ src/test/java/TestLeesXML.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Ivan on 09/05/2017.
@@ -30,11 +31,50 @@ public class TestLeesXML {
 
         ArrayList<ArrayList<Product>> expected = new ArrayList<ArrayList<Product>>();
         ArrayList<Product> expectedOrder1 = new ArrayList<Product>();
-        expectedOrder1.add(new Product("naam",new Locatie(1,2),6));
+        expectedOrder1.add(new Product("naam",new Locatie(3,4),6));
         expected.add(expectedOrder1);
 
+        AssertArrayArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void LeesXML_EenOrder(){
+        File file = new File("src/test/java/MockOrderEenOrder.xml");
+        manager.LeesXmlOrder(file);
+        ArrayList<ArrayList<Product>> actual = simulatorManager.getOrder();
+
+        ArrayList<ArrayList<Product>> expected = new ArrayList<ArrayList<Product>>();
+        ArrayList<Product> expectedOrder1 = new ArrayList<Product>();
+        expectedOrder1.add(new Product("naam",new Locatie(3,4),6));
+        expectedOrder1.add(new Product("naam",new Locatie(2,8),4));
+        expectedOrder1.add(new Product("naam",new Locatie(3,2),1));
+        expectedOrder1.add(new Product("naam",new Locatie(6,8),3));
+        expected.add(expectedOrder1);
+
+        AssertArrayArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void LeesXML_MeerdereOrder(){
+        File file = new File("src/test/java/MockOrderMeerdereOrder.xml");
+        manager.LeesXmlOrder(file);
+        ArrayList<ArrayList<Product>> actual = simulatorManager.getOrder();
+
+        ArrayList<ArrayList<Product>> expected = new ArrayList<ArrayList<Product>>();
+        ArrayList<Product> expectedOrder1 = new ArrayList<Product>();
+        expectedOrder1.add(new Product("naam",new Locatie(3,4),6));
+        expectedOrder1.add(new Product("naam",new Locatie(2,8),4));
+        ArrayList<Product> expectedOrder2 = new ArrayList<Product>();
+        expectedOrder2.add(new Product("naam",new Locatie(3,2),1));
+        expectedOrder2.add(new Product("naam",new Locatie(6,8),3));
+        expected.add(expectedOrder1);
+        expected.add(expectedOrder2);
+
+        AssertArrayArrayEquals(expected,actual);
+    }
+
+    private void AssertArrayArrayEquals(ArrayList<ArrayList<Product>> expected,ArrayList<ArrayList<Product>> actual){
         for(int i = 0; i<expected.size();i++){
-            System.out.println("--");
             for(int b = 0; b<expected.get(i).size();b++){
                 Assert.assertEquals(expected.get(i).get(b).getHoogte(),actual.get(i).get(b).getHoogte());
                 Assert.assertEquals(expected.get(i).get(b).getLocatie().getX(),actual.get(i).get(b).getLocatie().getX());
@@ -42,18 +82,5 @@ public class TestLeesXML {
 
             }
         }
-
-    }
-
-    @Test
-    public void LeesXML_EenOrder(){
-        File file = new File("src/test/java/MockOrderEenOrder.xml");
-        manager.LeesXmlOrder(file);
-    }
-
-    @Test
-    public void LeesXML_MeerdereOrder(){
-        File file = new File("src/test/java/MockOrderMeerdereOrder.xml");
-        manager.LeesXmlOrder(file);
     }
 }

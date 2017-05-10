5e4aa9377636e0723169d68c96d32ee471ab4fc6
Aaron van de Bunt <aaron.vd.bunt@outlook.com>
2017-05-10 16:05:40 +0200

    no message

--- src/main/java/SimulatorTSP/Algoritmes/_2Opt.java
+++ src/main/java/SimulatorTSP/Algoritmes/_2Opt.java
@@ -5,16 +5,19 @@ import simulatorTSP.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.*;
 
 /**
  * Created by Ivan on 04/05/2017.
  */
 public class _2Opt extends Algoritme{
-
+//bron: http://www.technical-recipes.com/2012/applying-c-implementations-of-2-opt-to-travelling-salesman-problems/
 
     public ArrayList<Product> BerekenRoute(ArrayList<Product> order) {
+        //bewaarde productlocaties
         ArrayList<Locatie> productlocaties = new ArrayList<Locatie>();
 
+        //bewaarde lengtes van afstanden
         List<Double> lengtes = new ArrayList<Double>();
 
         //stop productlocaties in array
@@ -23,29 +26,35 @@ public class _2Opt extends Algoritme{
             productlocaties.add(locatie);
         }
 
-        //krijg lengtes van locaties
-        for(Locatie productlocatie: productlocaties){
-            double lengte = productlocatie.getLengte();
+        //krijg lengtes van afstanden tussen locaties
+        for (int x = 1; x < productlocaties.size(); x++) {
+            lengtes.add((productlocaties.get(x).getLengte(productlocaties.get(x - 1))));
         }
 
+        double totaleAfstand = 0;
 
-
-
-        int grootte = productlocaties.size();
+        for(double x : lengtes){
+            totaleAfstand += x;
+        }
 
         int verbeter = 0;
 
         while(verbeter < 20){
-            double besteAfstand = productlocaties.size();
 
-            for(int i = 0; i < grootte-1; i++){
-                for(int k = i + 1; k < grootte; k++){
+            double besteAfstand = totaleAfstand;
+
+            for(int i = 0; i < totaleAfstand-1; i++){
+                for(int k = i + 1; k < totaleAfstand; k++){
+
                     wissel2Opt(i, k);
 
-                    double nieuweAfstand = productlocaties.
+                    double nieuweAfstand = totaleAfstand;
+
+                    //if()
                 }
             }
         }
+
         return null;
         }
 
